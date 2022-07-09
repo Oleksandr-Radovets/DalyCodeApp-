ebd856b6e677717f7805b8d3e77f5161dff49aca
sasha <radovietsoleksandr@gmail.com>
2022-07-09 19:27:23 +0300

    First Commit App

---
+++ src/main/java/com/example/dalycodeapp/DalyCodeAppApplication.java
@@ -0,0 +1,16 @@
+package com.example.dalycodeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalyCodeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DalyCodeAppApplication.class, args);
+
+
+
+
+    }
+}
---
+++ src/test/java/com/example/dalycodeapp/DalyCodeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dalycodeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalyCodeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
